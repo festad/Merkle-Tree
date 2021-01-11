c118fdb1cbfb9e71177c966c86c34a23b7349aa0
festad <d.festa004@studenti.unibs.it>
2021-01-11 15:40:09 +0000

    just added some comment and changed _hash to return an hash instead of str(hash)

--- merkle_tree.py
+++ merkle_tree.py
@@ -6,11 +6,18 @@ def _hash(string):
     if string == "":
         return ""
     else:
-        return str(sha256(str.encode(string)).hexdigest())
+        return sha256(str.encode(string)).hexdigest()
 
 
 class MerkleTree:
     def __init__(self, messages):
+        # You instantiate the tree passing to the
+        # constructor an array of strings:
+        # for example:
+        #     mt = MerkleTree(['First message', 'Second message', '...and the third'])
+        # The tree is implemented with an array, if you want to see it, call
+        #     mt._merkle_tree
+
         _n_leaves = 1
         self._height = 0
         n_messages = len(messages)
@@ -65,23 +72,34 @@ class MerkleTree:
         return self._merkle_tree[self._get_right_child_index(node_index)]
 
     def recursive_evaluation(self, current_node_index=0):
+        # Attention:
+        # whereas stack evaluation (next function) doesn't
+        # change the tree itself, this one does change, so calling
+        # this function without reassigning the tree results
+        # in getting different values for the root each time
         if self._has_left_child(current_node_index):
-            self._merkle_tree[current_node_index] = _hash(
+            self._merkle_tree[current_node_index] = str(_hash(
                 self.recursive_evaluation(self._get_left_child_index(current_node_index))
-                + self.recursive_evaluation(self._get_right_child_index(current_node_index)))
+                + self.recursive_evaluation(self._get_right_child_index(current_node_index))))
             return self._merkle_tree[current_node_index]
         else:
-            self._merkle_tree[current_node_index] = _hash(self._merkle_tree[current_node_index])
+            self._merkle_tree[current_node_index] = str(_hash(self._merkle_tree[current_node_index]))
             return self._merkle_tree[current_node_index]
 
     def stack_evaluation(self):
+        # Attention:
+        # this function doesn't change the values of the tree
+        # itself, but the previous one does. If you want to
+        # compare the results provided by
+        # stack_evaluation and recursive_evaluation, it's easier to
+        # call stack_evaluation first
         stack = []
         for k in range(int(pow(2, self._height)) - 1, len(self._merkle_tree)):
-            node_1 = (_hash(self._merkle_tree[k]), self._get_height_of_node(k))
+            node_1 = (str(_hash(self._merkle_tree[k])), self._get_height_of_node(k))
             if len(stack) > 0:
                 while node_1[1] == stack[len(stack) - 1][1]:
                     node_2 = stack.pop()
-                    node_1 = (_hash(node_2[0] + node_1[0] ), node_1[1] + 1)
+                    node_1 = (str(_hash(node_2[0] + node_1[0] )), node_1[1] + 1)
                     if len(stack) == 0:
                         break
             stack.append(node_1)
